7932d75a55d7595e23e39b8d71fea408690f5d9e
Marcelo Patricio <marcelo@patricio.net.br>
2026-06-12 19:39:54 -0300

    feat: feat: implement dynamic header and footer loading using async fetch

---
+++ src/js/utils.mjs
@@ -0,0 +1,25 @@
+export function renderWithTemplate(template, parentElement, data, callback) {
+  // If need to clear, clean the parent HTML first
+  parentElement.innerHTML = template;
+
+  if (callback) {
+    callback(data);
+  }
+}
+
+async function loadTemplate(path) {
+  const response = await fetch(path);
+  const template = await response.text();
+  return template;
+}
+
+export async function loadHeaderFooter() {
+  const headerTemplate = await loadTemplate('partials/header.html');
+  const headerElement = document.getElementById('main-header');
+  renderWithTemplate(headerTemplate, headerElement);
+  //   updateCartCountBadge();
+
+  const footerTemplate = await loadTemplate('partials/footer.html');
+  const footerElement = document.getElementById('main-footer');
+  renderWithTemplate(footerTemplate, footerElement);
+}
