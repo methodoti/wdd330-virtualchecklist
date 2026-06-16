e4ba1d1a88a6a3d93ee26353fa924ea125bc7fd8
Marcelo Patricio <marcelo@patricio.net.br>
2026-06-16 02:33:55 -0300

    feat: add aircraft page and syle

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'src/index.html'),
         checklist: resolve(__dirname, 'src/checklist/index.html'),
+        add_aircraft: resolve(__dirname, 'src/add_aircraft/index.html'),
         // add new pages as needed here!
       },
     },
