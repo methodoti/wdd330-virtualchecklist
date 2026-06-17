47b60db32fa1c4c016896b58ef4539fa5e442890
Marcelo Patricio <marcelo@patricio.net.br>
2026-06-17 01:13:39 -0300

    feat: last flight on main page

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ import getChecklistData from './AircraftData.mjs';
 import { initSimBrief } from './SimBriefData.mjs';
 import { initWeather } from './WeatherData.mjs';
 import { initCheckList } from './ChecklistRender.mjs';
-import { renderListWithTemplate, loadHeaderFooter } from './utils.mjs';
+import { renderListWithTemplate, renderWithTemplate, loadHeaderFooter } from './utils.mjs';
 
 // load header and footer
 loadHeaderFooter();
@@ -29,6 +29,58 @@ function displayAircraftButtons(aircrafts) {
   );
 }
 
+function lastFlightTemplate(flightData) {
+    // <div class="lastflightDiv cards">
+    //     <div id="lasflight-img" class="lastflightImg" style="background-image: url(&quot;images/b737-800.webp&quot;); background-size: cover; background-position: center center;"><div></div></div>
+    //     <div class="lastflightAircraft">
+    //         <p class="lastflightP ">Aircraft: <span id="last-aircraft">Boeing 737-800</span></p>
+    //         <p>Date: <span id="last-date">Jun 15, 2026</span></p>
+    //     </div>
+    // </div>
+  return `
+        <div id="lasflight-img" class="lastflightImg" style="background-image: url('/images/aircrafts/${flightData.id}.webp'); background-size: cover; background-position: center center;"><div></div></div>
+        <div class="lastflightAircraft">
+            <p class="lastflightP ">Aircraft: <span id="last-aircraft">${flightData.name}</span></p>
+            <p>Date: <span id="last-date">${flightData.date}</span></p>
+        </div>
+    `;
+}
+
+function displayLastFlight() {
+
+    const lastFlightData = JSON.parse(localStorage.getItem("lastFlight"));
+    if (!lastFlightData) {
+        registerLastFlight();
+    }
+    // console.log(lastFlightData);
+    // const lastflightSection = document.querySelector(".lastflightSection");
+    const lastFlightDiv = document.querySelector('.lastflightDiv');
+
+    if (!lastFlightDiv) {
+        registerLastFlight();
+    }
+       // return; // good practice!
+  
+    const htmlStrings = lastFlightTemplate(lastFlightData);
+    // const headerTemplate = await loadTemplate('/partials/header.html');
+    //   const headerElement = document.getElementById('main-header');
+      // Passes the callback function setMenuCurrent (without parenteses for not running it now)
+    renderWithTemplate(htmlStrings, lastFlightDiv);
+
+}
+
+// função temporaria! só para poder testar o template e a página
+function registerLastFlight() {
+            const currentDate = new Date().toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' });
+            const lastFlight = {
+                id: "b737-800",
+                name: "Boeing 737-800",
+                date: currentDate
+            };
+            localStorage.setItem("lastFlight", JSON.stringify(lastFlight));
+        }
+
+
 // ==========================================
 // INITIALIZERS
 // ==========================================
@@ -43,6 +95,8 @@ async function init() {
   // init Aircraft data
   const aircrafts = await initAircraftData();
   displayAircraftButtons(aircrafts);
+  displayLastFlight();
+
 
   //init dummy functions
   initSimBrief();
