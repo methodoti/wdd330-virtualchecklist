8ffb7acb983e39a867307dfc04e07e69e2016080
Marcelo Patricio <marcelo@patricio.net.br>
2026-06-17 19:56:12 -0300

    feat: implement checklist layout and view rendering with dynamic templates

--- src/js/ChecklistRender.mjs
+++ src/js/ChecklistRender.mjs
@@ -1,3 +1,177 @@
+import { renderWithTemplate, renderListWithTemplate } from './utils.mjs';
+
 export function initCheckList() {
   console.log('Checklist Module Loaded');
+}
+
+// ==========================================
+// TEMPLATES
+// ==========================================
+
+function aircraftNameTemplate(aircraft) {
+  return `
+    <h2 class="aircraftName">${aircraft}</h2>
+  `;
+}
+
+function resetAllButtonTemplate() {
+  return `
+    <button class="resetAll buttons buttonsRounded">Reset All</button>
+  `;
+}
+
+function resetButtonTemplate() {
+  return `
+    <div class="resetDiv"><button class="reset buttons buttonsRounded">Reset</button></div>
+  `;
+}
+
+function categoryTitleTemplate(category) {
+  return `
+    <div class="categoryTitleDiv cardTitle" aria-label="${category}">
+      <h3 class="cardTitle">${category}</h3>
+      <span class="categoryButton"></span>
+    </div>
+  `;
+}
+
+// case Checkpoint
+function checkpointTemplate(item) {
+  return `
+    <div class="checklistItem type-checkpoint">
+        <div class="check">
+          <div class="checkIcon"></div>
+        </div>
+        <div class="item type-checkpoint">
+            <span class="task">${item.task}</span>
+            <div class="dot"></div>
+            <span class="action">${item.action}</span>
+        </div>
+    </div>`;
+}
+
+// case Info
+function infoTemplate(item) {
+  return `
+    <div class="checklistItem type-info">
+      <div class="check">
+        <div class="checkIcon"></div>
+      </div>
+      <div class="item type-info">
+        <span class="task">${item.task}</span>
+        <div class="dot"></div>
+        <span class="action">${item.action}</span>
+      </div>
+    </div>`;
+}
+
+// Case Subtitle
+function subtitleTemplate(item) {
+  return `
+    <div class="checklistItem type-subtitle">
+        <div class="item type-subtitle">
+            <span class="task">${item.text}</span>
+        </div>
+    </div>`;
+}
+
+// Case SimAction
+function simActionTemplate(item) {
+  return `
+    <div class="checklistItem type-sim-action">
+      <div class="check">
+        <div class="checkIcon"></div>
+      </div>
+      <div class="item type-sim-action">
+        <span class="task">${item.task}</span>
+      </div>
+    </div>`;
+}
+
+// Case ATC
+function AtcTemplate(item) {
+  return `
+    <div class="checklistItem type-checkpoint">
+      <div class="check">
+        <div class="checkIcon"></div>
+      </div>
+      <div class="item type-checkpoint type-atc">
+        <span class="task">${item.task}</span>
+        <div class="dot"></div>
+        <span class="action">${item.action}</span>
+      </div>
+    </div>`;
+}
+
+function itemRender(item) {
+  switch (item.type) {
+    case 'checkpoint':
+      return checkpointTemplate(item);
+    case 'info':
+      return infoTemplate(item);
+    case 'subtitle':
+      return subtitleTemplate(item);
+    case 'sim-action':
+      return simActionTemplate(item);
+    case 'atc':
+      return AtcTemplate(item);
+    default:
+      console.log(`Not Found: ${item.type}`);
+      return '';
+  }
+  // // console.log(item);
+  // const { type, task, action } = item;
+
+  // if (type === 'checkpoint') {
+  //   console.log(item.type);
+  //   return checkpointTemplate(item);
+  // } else if (type === 'info') {
+  //   return infoTemplate(item);
+  // } else if (type === 'subtitle') {
+  //   return subtitleTemplate(item);
+  // } else if (type === 'sim-action') {
+  //   return simActionTemplate(item);
+  // } else if (type === 'atc') {
+  //   return AtcTemplate(item);
+  // }
+}
+
+// ==========================================
+// RENDER
+// ==========================================
+
+export function displayChecklist(plane, parentElement) {
+  // console.log(plane);
+
+  let checklistPecasHTML = [];
+  // // Aircraft Name for <h2>
+
+  const titleHTML = aircraftNameTemplate(plane.aircraftName);
+  checklistPecasHTML.push(titleHTML);
+  // parentElement.insertAdjacentHTML('beforeend', titleHTML);
+
+  // // Reset All Button
+  const resetAllButtonHTML = resetAllButtonTemplate();
+  checklistPecasHTML.push(resetAllButtonHTML);
+
+  plane.checklists.forEach((element) => {
+    const categoryTitleHTML = categoryTitleTemplate(element.categoryName);
+    checklistPecasHTML.push(categoryTitleHTML);
+
+    checklistPecasHTML.push('<div class="checklistDiv cards">');
+    element.items.forEach((item) => {
+      const itemHTML = itemRender(item);
+      checklistPecasHTML.push(itemHTML);
+    });
+
+    const resetButtonHTML = resetButtonTemplate();
+    checklistPecasHTML.push(resetButtonHTML);
+
+    checklistPecasHTML.push('</div>');
+  });
+
+  checklistPecasHTML.push(resetAllButtonHTML);
+
+  document.querySelector('.categorySection').innerHTML =
+    checklistPecasHTML.join('');
 }
